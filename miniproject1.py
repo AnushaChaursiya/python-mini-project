e6f8936a749a9b230cc0c814611475d2b2868fcf
AnushaChaursiya <anushachaursiya7737@gmail.com>
2025-02-10 16:34:38 +0530

    mini project

--- miniproject1.py
+++ miniproject1.py
@@ -1,1 +1,15 @@
+import random
+
+while True:
+    choice = input('roll the dice(y/n):').lower()
+    if choice == 'y':
+        diel1 = random.randint(1,6)
+        diel2 = random.randint(1,6)
+        print(f'({diel1}, {diel2})')
+    elif choice == 'n':
+        print('Thankyou for playing!')
+        break
+    else:
+        print('Invalid choice!')
+
 
